2d531df797c91e99a6b76f12fa7c43ccb4b219f7
stan-2021131 <sergiocoromac@gmail.com>
2025-08-12 08:31:56 -0600

    TextField y Button para agregar tarea

--- app/src/main/java/com/example/lab3/MainActivity.kt
+++ app/src/main/java/com/example/lab3/MainActivity.kt
@@ -4,13 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.lab3.ui.theme.Lab3Theme
 
 class MainActivity : ComponentActivity() {
@@ -24,6 +36,7 @@ class MainActivity : ComponentActivity() {
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
+                    AgregarTarea()
                 }
             }
         }
@@ -36,6 +49,32 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+}
+
+@Composable
+fun AgregarTarea(){
+    var tarea by remember { mutableStateOf("") }
+    Row(
+        modifier = Modifier.padding(vertical = 30.dp),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        TextField(
+            value = tarea,
+            onValueChange = {tarea = it},
+            singleLine = false,
+            label = { Text(stringResource(R.string.agregarTarea)) },
+            modifier = Modifier.width(270.dp).padding(15.dp)
+        )
+        Button(
+            onClick = {
+
+            }
+        ) {
+            Text(stringResource(R.string.botonAgregar))
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
