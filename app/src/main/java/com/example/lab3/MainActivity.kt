157c19c97a44e8f3f6a5b7f1ef894920625932da
Jackelyn <jackelyngironvillacinda@gmail.com>
2025-08-12 08:24:56 -0600

    Se agrego imagen de fondo

--- app/src/main/java/com/example/lab3/MainActivity.kt
+++ app/src/main/java/com/example/lab3/MainActivity.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.res.painterResource
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.lab3.ui.theme.Lab3Theme
 
@@ -38,10 +44,25 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+@Composable
+fun GreetingImage( modifier: Modifier = Modifier){
+    val image = painterResource(R.drawable.fondopanda)
+    Box(modifier){
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.6F
+        )
+    }
+
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Lab3Theme {
+        GreetingImage()
         Greeting("Android")
     }
 }
